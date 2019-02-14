92345c0d1a7bb599aa56d32828e3ba8c92f6487c
luzhipeng <luzhipeng@duiba.com.cn>
2019-02-14 14:09:43 +0800

    test: unit test

--- index.test.js
+++ index.test.js
@@ -1,19 +1,23 @@
-var postcss = require('postcss');
+var postcss = require("postcss");
 
-var plugin = require('./');
+var plugin = require("./");
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+    return postcss([plugin(opts)]).process(input);
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it("doesn nothing", () => {
+    return run("a{ }", {}).then(result => {
+        expect(result.css).toBe("a{ }");
+    });
 });
 
-*/
+it("does something", () => {
+    return run("a{ border-radius: 10px; overflow: hidden; }", "a{ }", {}).then(
+        result => {
+            expect(result.css).toBe(
+                "a{ border-radius: 10px; overflow: hidden; -webkit-mask-image: -webkit-radial-gradient(white, black); }"
+            );
+        }
+    );
+});
